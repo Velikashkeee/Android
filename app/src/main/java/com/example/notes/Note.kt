61fa91bea970610742c5077cf45d58fb7b8d0320
ArtemKiriutin <artem.kiriutin@duallab.com>
2020-02-25 22:28:45 +0300

    Hello nikita

---
+++ app/src/main/java/com/example/notes/Note.kt
@@ -0,0 +1,7 @@
+package com.example.notes
+
+class Note(nodeId: Int, nodeName: String, nodeDes: String) {
+    var nodeID: Int? = nodeId
+    var nodeName: String? = nodeName
+    var nodeDes: String? = nodeDes
+}
